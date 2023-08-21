0467db3c9e444a92d5c3e8eac2a4e541639393ef
mattvitello <matt.vitello@cosmos.space>
2023-08-21 15:25:11 -0400

    fix cors

--- src/pages/api/movie/log.ts
+++ src/pages/api/movie/log.ts
@@ -1,17 +1,20 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
+import cors from "nextjs-cors";
 
 const logMovieHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await cors(req, res);
+
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
   const ctx = createTRPCContext({ req, res });
   const caller = appRouter.createCaller(ctx);
-  await caller.movie.getCount();
+  const numberOfMovies = await caller.movie.getCount();
 
-  return res.status(200).json(req.body);
+  return res.status(200).json({...req.body, numberOfMovies});
 };
 
 export default logMovieHandler;
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,25 +1,17 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
-import cors from "nextjs-cors";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  await cors(req, res);
-
-  return createNextApiHandler({
-    router: appRouter,
-    createContext: createTRPCContext,
-    onError:
-      env.NODE_ENV === "development"
-        ? ({ path, error }) => {
-            console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-            );
-          }
-        : undefined,
-  });
-};
-
-export default handler;
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+          );
+        }
+      : undefined,
+});
