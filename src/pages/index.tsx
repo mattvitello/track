c2dbd12a315db9ffbbcfcc7efcdc0a3833b054e9
mattvitello <matt.vitello@cosmos.space>
2023-09-06 14:51:22 -0400

    add link to main website, add logo, and slight layout style adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { type NextPage } from "next/types";
 
 const Home: NextPage = () => {
   return (
-    <div className="my-4 sm:my-12 lg:mx-8 lg:my-20 text-md items-center">
+    <div className="pb-4 my-4 sm:my-12 lg:mx-8 lg:my-20 text-md items-center">
       <p className="mb-4">
         {`For the past few years, I've been logging and aggregating data about my
         consumption habits from two primary sources: `}
@@ -39,8 +39,8 @@ const Home: NextPage = () => {
         {`In a broader sense, my goal here is to observe how I invest my time,
         capture my experiences, identify what was most present
         with me during different junctures of my life, and utilize this
-        information to perform interesting analysis on myself such as predicting my behaviors, 
-        or informing future decisions. Currently, the most conveniently accessible
+        information to perform analysis on myself such as predicting my habits/behaviors, 
+        or informing future decisions I make. Currently, the most conveniently accessible
         data pertains to just films and music, which I primarily use for simple
         nostalgic "What was last year like?" purposes. Despite the contrast between this use case and my grander aspirations, these initial
         datasets lay the foundation for this system which will
